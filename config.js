9479f739c91225870b11e8489a32e8ff40eca234
Taj426 <tajiadam823@gmail.com>
2024-12-25 10:28:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ global.github = process.env.GITHUB || "https://github.com/Frecyber/Frecyber-md";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/1d4a42b28790d0f7a7195.jpg";
-global.devs = "255620814108,255764182801,255776663848,255752593977";
-global.sudo = process.env.SUDO || "255620814108,255764182801,255776663848,255752593977";
-global.owner = process.env.OWNER_NUMBER || "255784686209,255745086279";
+global.devs = "255674402466";
+global.sudo = process.env.SUDO || "255674402466";
+global.owner = process.env.OWNER_NUMBER || "255674402466";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "true";
 global.wlcm = process.env.WELCOME || "true";
@@ -45,14 +45,14 @@ global.SESSION_ID =
   "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkN5RDhHaEhIWGk0RjdFc2RzUHl6TzhidnRXWEdCNW0zc0U4ZGo0Y1czND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQXo0WDlrOFMzVFBURUxZbEI2QnVsbmJuYXFkOU02eFltbEdPbUtUQ3B3VT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJvRVdZWHdoWkJ6NFc4YzRzVWpyUCtqenRvTUxUT1BEcUtxaEV1Syt5VGtFPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJuUG9MSEs0cU0vTEZQZTBNaU1Sc0xMRmkzaWxCL0cwL2RVU05HWE9KYzBVPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhPSHhrdGppb1ovZDZ3YVd3YTlBZnhxOXFmTFRqL1ZSQkl5enB2bi9Bblk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ikp6eWVDTDlPNU5BYXdnVG1HcFhidGkvRmYxY0hieGIxcTFML0t0ZXZQMjg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieU03S3lpNjVQVlFxdXk5R3FUUk9BZXBpRnJxNVd2aXVwK2VQRnRnbjJtdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU1UrMVZ3c003QVJWZEl2UTlHV3BGUnc3SGJQcXhpcFByV0dTNnExQlN4VT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlMvV3ZyTFN4Z09wOVZYWStRRWlFcTVQaDAyaE55ZnlsZS9RQ3BvaTB5REw3eEZVNTJWdHliQnVGRmdnMjFSN0N1R1hKYXQwUHBhZ3paaHVHOTZHU0FBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjI1LCJhZHZTZWNyZXRLZXkiOiIxQnFTNnRXUDB0S1k2cFl4cnJzbk13ZGxYNVNxOHBXMHRiTCtCMWhTT1VzPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJpRE93QVlValFjaU9EZzRzdy15TjRBIiwicGhvbmVJZCI6IjlmM2EwMmI3LWM1MjUtNGIyMi1iZGQ1LTJlMjNjYmIwNWNjNCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJSb2FCTkZWNlI1QTNxdm5wQ2J6NjNoUDZvSjQ9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR2QzUktIVjZJdVpBaHdRL0RRZVcyYU5IdlNFPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ik1BU1A3R1RKIiwibWUiOnsiaWQiOiIyNTU3ODQ2ODYyMDk6MzBAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoiTmFqdW5hNzcifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0ozd3FZVURFSW5qc0xjR0dBSWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IklWMko4OEZ5aUsyM0I2NklBRWVZM2JmNHFBeVVEWW1DMTVrZ1YvMmMrQ2s9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlBsQUw5VXl3T0x4eGo0Qjc5em9uNEdhc0Q0c2kyMzZzckVNOS9IU3lFdnRPajduZkVSK0djY1gxM3hXRzlsejdheWF2WDlWSTA2Yys1dXdTMGFVbUFBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiI0c3hkNGZleWhLZzgxenZlUVQ3bWpVcWM5cWVRalFFOFIralZmMDN2b2pjcUl4QlFVcXIzZmZTU0pYU1JNUzJKcnF4UUFxa0VyMnRQdjNKVXh3ME9DQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NTc4NDY4NjIwOTozMEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJTRmRpZlBCY29pdHR3ZXVpQUJIbU4yMytLZ01sQTJKZ3RlWklGZjluUGdwIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzI2NzU1MjIzfQ=="
 module.exports = {
   menu: process.env.MENU || "",
-  HANDLERS: process.env.PREFIX || ".",
+  HANDLERS: process.env.PREFIX || ",",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "you're using first powerful bot to be released in Tanzania| enjoy your time| this is 𝐅𝚪𝚵𝐂𝐘𝚩𝚵𝚪 𝚳𝐃",
-  author: process.env.PACK_AUTHER || "FRECYBER",
+  author: process.env.PACK_AUTHER || "༒𝐃.𝐉༒",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "FRECYBER-𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "FRECYBER",
+  botname: process.env.BOT_NAME || "༒𝐃.𝐉༒-𝐌𝐃",
+  ownername: process.env.OWNER_NAME || "༒𝐃.𝐉༒",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
